84a0c606e7f786aa1485e1f827d72d079578fea4
Omar <omarossama1029@gmail.com>
2026-07-14 12:43:50 +0300

    Extracted name, price, and all product images, from 1 product

--- nothing-personal.py
+++ nothing-personal.py
@@ -8,3 +8,17 @@ response = requests.get(url).text
 soup = BeautifulSoup(response, 'lxml')
 
 productList = soup.find_all('li', class_ = 'product-grid__item')
+
+name = productList[0].find('h3').text
+
+print(name)
+
+price = productList[0].find('span', class_ = 'price').text.strip()
+
+print(price)
+
+productGallery = productList[0].find_all('div', class_ = 'product-media')
+
+for div in productGallery:
+    imgUrl = div.find('img')['src']
+    print('https:' + imgUrl)
