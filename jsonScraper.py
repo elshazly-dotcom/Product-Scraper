61e8d7e2e769b782277dc428c16c4ab6bc1efd9e
Omar <omarossama1029@gmail.com>
2026-07-18 16:56:37 +0300

    mvp for the json scraper

--- jsonScraper.py
+++ jsonScraper.py
@@ -1,19 +1,29 @@
 import requests
 import json
+from fake_useragent import UserAgent
 
 url = input("Enter URL: ")
+
+name = input("Name of file: ")
+
+if url[-1] == '/':
+    url = url[:-1]
 
 if "collections" in url:
     url += "/products.json"
 else:
     url += "/collections/all/products.json"
 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"}
+ua = UserAgent()
+
+headers = {"User-Agent": ua.random}
 
 response = requests.get(url, headers= headers)
 
 if response.status_code == 200:
     data = response.json()
 
-    with open ("website.json", "w") as f:
+    with open (f"{name}.json", "w") as f:
         json.dump(data, f, indent=4)
+else:
+    print(response.status_code)
