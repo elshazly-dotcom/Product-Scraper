87b2e8df669e70ec192129f93cbf535b57a34993
Omar <omarossama1029@gmail.com>
2026-07-20 20:53:50 +0300

    idk what happened

--- main.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://www.nothing-personal.com/collections/tops"
-
-response = requests.get(url)
-
-soup = BeautifulSoup(response.text, 'lxml')
