a6466eaec371b515ff3c05b704d0391c4e4c8aaf
Omar <omarossama1029@gmail.com>
2026-07-31 01:04:17 +0300

    finished extract function

--- cacher.py
+++ cacher.py
@@ -1,31 +1,68 @@
 import json
-import csv
+
+def extract_product(product):
+    vIDtoColor = {}
+
+    for i, option in enumerate(product["options"]):
+                if option["name"] == "Color":
+                    colorPosition = i + 1
+                    colorOption = option
+                elif option["name"] == "Size":
+                    sizePosition = i + 1
+
+    for variant in product["variants"]:
+        vID = variant["id"]
+        color = variant[f"option{colorPosition}"]
+        vIDtoColor[vID] = color
+
+    colors = colorOption["values"]
+
+    resultColors = []
+
+    for color in colors:
+        matchingVariants = []
+        for i in product["variants"]:
+            if vIDtoColor[i["id"]] == color:
+                matchingVariants.append(i)
+
+        sizes=[]
+        matchingVariantsIds = []
+        for j in matchingVariants:
+            matchingVariantsIds.append(j["id"])
+            sizes.append({
+                "size": j[f"option{sizePosition}"],
+                "price": j["price"],
+                "compare_at_price": j["compare_at_price"],
+                "available": j["available"]
+            })
+
+        matchingImages = []
+        for img in product["images"]:
+           if not img["variant_ids"]:
+                matchingImages.append(img["src"])
+           else:
+               for vId in img["variant_ids"]:
+                if vId in matchingVariantsIds:
+                    matchingImages.append(img["src"])
+                    break
+
+        resultColors.append({
+            "color": color,
+            "sizes": sizes,
+            "images": matchingImages
+        })
 
 
-with open ("nothing-personal.json", "r") as f:
-    data = json.load(f)
-    for i, product in enumerate(data["products"]):
-        print(f"PRODUCT NUMBER {i}")
+    return{
+        "id": product["id"],
+        "title": product["title"],
+        "colors": resultColors
+    }     
+             
+def main():
+    with open ("theblanks.json", "r") as f:
+        data = json.load(f)
+        product1 = data["products"][0]
+        print(extract_product(product1))
 
-        productName = product["title"]
-        print(f"Name: {productName}")
-        
-        productType = product["product_type"]
-        print(f"Type: {productType}")
-
-        sizes = []
-        for variant in product["variants"]:
-            sizes.append(variant["title"])
-        
-        print(f"Sizes: {sizes}")
-
-        price = variant["price"]
-        print(f"Price: {price}")
-
-        imgs = []
-        for i, img in enumerate(product["images"]):
-            imgs.append(img["src"])
-
-        print(f"Imgs: {imgs}")
-        print()
-    
+main()
